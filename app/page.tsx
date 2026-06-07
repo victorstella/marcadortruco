64dde08bf53b6dc68ff624f5d7c1cee102dff383
victorstella <victor.stella96@gmail.com>
2026-06-07 18:25:27 -0300

    link to github project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { GithubLink } from '@/components/GithubLink';
 import { Scoreboard } from '@/components/Scoreboard';
 
 export default function Home() {
   return (
     <main>
       <Scoreboard />
+      <GithubLink />
     </main>
   );
 }
---
+++ components/GithubLink.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export function GithubLink() {
+  return (
+    <div className="flex w-full justify-center mt-4">
+      <button
+        className="bg-gray-900 rounded-md px-2 py-1 text-gray-400 border border-gray-500 hover:border-teal-800 hover:text-teal-400 cursor-pointer"
+        onClick={() =>
+          window.open('https://github.com/victorstella/marcadortruco', '_blank')
+        }
+      >
+        GitHub
+      </button>
+    </div>
+  );
+}
